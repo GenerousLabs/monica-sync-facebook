d633ea811373fd65bebaf338b1a0af8a1d987884
Callum Macdonald <git@callum-macdonald.com>
2021-11-18 22:18:03 +0100

    Add settings to backup page.

--- extension/src/backup/backup.ts
+++ extension/src/backup/backup.ts
@@ -1,10 +1,15 @@
 import { getFriends } from "../facebook/friends";
+import { getState } from "../state";
 import { getByIdOrThrow } from "../utils";
 
 const startBackup = async (doc: Document) => {
   const friends = await getFriends();
   getByIdOrThrow<HTMLInputElement>(doc, "friendsList").value =
     JSON.stringify(friends);
+
+  const { facebookFriendsToScrape, ...settings } = await getState();
+  getByIdOrThrow<HTMLInputElement>(doc, "settings").value =
+    JSON.stringify(settings);
 };
 
 globalThis.setTimeout(() => {
