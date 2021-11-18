00c819e6bbab4cc839f6a7bac7b339933e49eff6
Callum Macdonald <git@callum-macdonald.com>
2021-11-18 23:09:17 +0100

    Update comments.

--- extension/src/shared.config.ts
+++ extension/src/shared.config.ts
@@ -3,20 +3,21 @@ const STARTING_RATE_LIMIT_SECONDS = 7;
 /** How much should we increase the rate limit delay each time? */
 const RATE_LIMIT_DELAY_BACKUP_MULTIPLIER = 3;
 
+/** After scraping an about page, always wait this long before the random wait*/
+const ABOUT_PAGE_FIXED_DELAY_MS = 2e3;
 /** After we scrape an about page, how long do we wait before moving onto the
  * next page */
 const ABOUT_PAGE_RANDOM_DELAY_MS = 5e3;
-/** After scraping an about page, always wait this long */
-const ABOUT_PAGE_FIXED_DELAY_MS = 2e3;
 
-/** After we scrape an about page, how long do we wait before moving onto the
- * next page */
-const PROFILE_PAGE_RANDOM_DELAY_MS = 6e3;
-/** After scraping an about page, always wait this long */
+/** After we navigate to a profile page, always wait this long before the random
+ * delay */
 const PROFILE_PAGE_FIXED_DELAY_MS = 2e3;
+/** After we navigate to a profile page, how long do we wait before moving onto
+ * the next page */
+const PROFILE_PAGE_RANDOM_DELAY_MS = 6e3;
 
-/** After we scrape an about page, how long do we wait before moving onto the
+/** When we start a sccraping run, always wait this long */
+const GETTING_STARTED_FIXED_DELAY_MS = 2e3;
+/** When we start a scraping run, how long do we wait before moving onto the
  * next page */
 const GETTING_STARTED_RANDOM_DELAY_MS = 1e3;
-/** After scraping an about page, always wait this long */
-const GETTING_STARTED_FIXED_DELAY_MS = 2e3;
