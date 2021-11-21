1ed1271e47863e70db6af008fa992999feb8529e
Callum Macdonald <git@callum-macdonald.com>
2021-11-21 23:14:12 +0100

    Option to retry syncing.

--- extension/src/unmatched/unmatched.ts
+++ extension/src/unmatched/unmatched.ts
@@ -3,7 +3,8 @@ import {
   selectUnmatchedFriends,
   setFriendsToSyncToMonica,
 } from "../facebook/friends";
-import { getByIdOrThrow } from "../utils";
+import { syncFriendToMonica, tryToFindMonicaFriendId } from "../monica/monica";
+import { delay, getByIdOrThrow } from "../utils";
 
 const syncToMonicaFactory = (doc: Document) => async () => {
   const inputs = doc.querySelectorAll<HTMLInputElement>("input[type=checkbox]");
@@ -11,6 +12,22 @@ const syncToMonicaFactory = (doc: Document) => async () => {
   const profileUrlsToSync = checkedInputs.map((input) => input.value);
   await setFriendsToSyncToMonica(profileUrlsToSync);
   globalThis.alert("Friends set to sync. #0TF8GR");
+};
+
+const tryToSyncAgainToMonica = async () => {
+  const friends = await getFriends();
+  const unmatched = selectUnmatchedFriends(friends);
+
+  for (const friend of unmatched) {
+    const monicaId = await tryToFindMonicaFriendId({ friend });
+    if (typeof monicaId !== "undefined") {
+      // Wait 1s if we found a match to avoid monica rate limiting
+      await delay(1e3);
+      await syncFriendToMonica({ friend, monicaId });
+    }
+    // Wait between attempts to sync to avoid monica's rate limits
+    await delay(1e3);
+  }
 };
 
 const unmatchedStart = async (doc: Document) => {
@@ -43,6 +60,23 @@ const bindButtons = (doc: Document) => {
   } catch (error) {}
   try {
     getByIdOrThrow(doc, "syncToMonica").onclick = syncToMonicaFactory(doc);
+  } catch (error) {}
+  try {
+    getByIdOrThrow(doc, "retry").onclick = async () => {
+      globalThis.alert("Starting now. Please wait. #2HIdnr");
+      try {
+        await tryToSyncAgainToMonica();
+      } catch (error) {
+        globalThis.alert(
+          `Error: The process encountered an error. #yAlv47\n${
+            (error as Error)?.message
+          }`
+        );
+        return;
+      }
+      globalThis.alert("Finished. #iH6kP2");
+      globalThis.document.location.reload();
+    };
   } catch (error) {}
 };
 
